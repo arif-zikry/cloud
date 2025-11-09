95872eca1839e058d5853c085c37ee3194f5b348
arif-zikry <b122420108@student.utem.edu.my>
2025-11-10 01:13:08 +0800

    Further update for Week 3 (Final)

--- README.md
+++ README.md
@@ -32,3 +32,96 @@
 # Week 3
 
 1. POST 
+
+    - What HTTP status code is returned when a ride is created successfully? 
+
+    HTTP Status 201
+
+    - What is the structure of the response body?
+
+    JSON
+
+2. GET
+
+    - What happens if the rides collection is empty? 
+
+    The server returns an empty {}.
+
+    - What data type is returned in the response (array/object)? 
+
+    Array
+
+3. Fix PATCH and DELETE Error
+
+    - Catch the error when requesting PATCH or DELETE API, then try to fix the issue reported.
+
+    
+
+    - If you try to update a non-existent ride ID, what status code is returned? 
+
+    Server returns error code 500 (Internal Server Error)
+
+    - What is the vakue of updated in the response if the update succeeds
+
+    1
+
+    - How does the API differentiate between a successful deletion and a failed one?
+
+    The API reads the amount of deleted documents where 0 determines no deletion and other values mean a successful deletion
+
+4. Users Endpoints
+
+// Users
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await db.collection('users').find().toArray();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch users' });
+    }
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const result = await db.collection('users').insertOne(req.body);
+        res.status(201).json({ id: result.InsertedId });
+    } catch (err) {
+        res.status(500).json({ error: 'Invalid User Data' });
+    }
+});
+
+app.patch('/users/:id', async (req, res) => {
+    try {
+        const result = await db.collection('users').updateOne(
+            { _id: new ObjectId(req.params.id) },
+            { $set: { status: req.body.status } }
+        );
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.status(200).json({ updated: result.modifiedCount});
+
+    } catch (err) {
+        res.status(500).json({ error: 'Invalid User ID or data' });
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const result = await db.collection('users').deleteOne(
+            { _id: new ObjectId(req.params.id) }
+        );
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.status(200).json({ deleted: result.deletedCount});
+    
+    }   catch (err) {
+        res.status(500).json({ error: 'Invalid User ID' });
+    }
+});
+
+5. Refer to image in Week 3
--- Week 3/index.js
+++ Week 3/index.js
@@ -2,10 +2,12 @@
 
 const { MongoClient, ObjectId } = require('mongodb');
 const express = require('express');
+const path = require('path');
 const port = 3000;
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 let db;
 
@@ -28,12 +30,14 @@ async function ConnectToMongoDB() {
 ConnectToMongoDB();
 
 app.listen(port, () => {
-    console.log(`Express erver is running on port: ${port}`);
+    console.log(`Express server is running on port: ${port}`);
 });
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Ride Sharing Service API');
 });
+
+// Drivers
 
 app.get('/rides', async (req, res) => {
     try {
@@ -47,7 +51,7 @@ app.get('/rides', async (req, res) => {
 app.post('/rides', async (req, res) => {
     try {
         const result = await db.collection('rides').insertOne(req.body);
-        res.status(201).json({ id: result.InsertedId });
+        res.status(201).json({ id: result.insertedId });
     } catch (err) {
         res.status(500).json({ error: 'Invalid Ride Data' });
     }
@@ -83,5 +87,58 @@ app.delete('/rides/:id', async (req, res) => {
     
     }   catch (err) {
         res.status(500).json({ error: 'Invalid ride ID' });
+    }
+});
+
+// Users
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await db.collection('users').find().toArray();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch users' });
+    }
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const result = await db.collection('users').insertOne(req.body);
+        res.status(201).json({ id: result.InsertedId });
+    } catch (err) {
+        res.status(500).json({ error: 'Invalid User Data' });
+    }
+});
+
+app.patch('/users/:id', async (req, res) => {
+    try {
+        const result = await db.collection('users').updateOne(
+            { _id: new ObjectId(req.params.id) },
+            { $set: { status: req.body.status } }
+        );
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.status(200).json({ updated: result.modifiedCount});
+
+    } catch (err) {
+        res.status(500).json({ error: 'Invalid User ID or data' });
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const result = await db.collection('users').deleteOne(
+            { _id: new ObjectId(req.params.id) }
+        );
+
+        if (result.deletedCount === 0) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+        res.status(200).json({ deleted: result.deletedCount});
+    
+    }   catch (err) {
+        res.status(500).json({ error: 'Invalid User ID' });
     }
 });
